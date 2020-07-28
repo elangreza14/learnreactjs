97081ea575e6e767e323b9867c31c26f3eacf6bf
indo.office3 <indo.office3@kasumi.co.jp>
2020-07-28 19:46:28 +0700

    update modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button/Button";
 import "./PlaceItem.css";
+import Modal from "../../shared/components/UIElements/Modal";
 const PlaceItem = (props) => {
-  let { image, title, address, description,id } = props.item;
+  let { image, title, address, description, id } = props.item;
+  const [show, setShow] = useState(false);
+  const openHandler = () => setShow(true);
+  const closeHandler = () => setShow(false);
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={image} alt={title}></img>
+    <React.Fragment>
+      <Modal
+        show={show}
+        onCancel={closeHandler}
+        header={address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP!!!!!</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`edit/places/${id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={image} alt={title}></img>
+          </div>
+          <div className="place-item__info">
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`edit/places/${id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Modal.css";
+import BackDrop from "./BackDrop";
+import { CSSTransition } from "react-transition-group";
 
-const Modal = () => {
-    return (
-        <div>
-            
+const ModalOverlay = (props) => {
+  return ReactDOM.createPortal(
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.header}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
         </div>
-    )
-}
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>,
+    document.getElementById("modal-hook")
+  );
+};
 
-export default Modal
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {props.show && <BackDrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={500}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
